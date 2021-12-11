5a067fa5379c6371e06a395d9a6bc668715505f4
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-12-11 09:41:10 +0400

    feat: web scraper for copying page html

---
+++ Scraper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://yts.mx/");
+            foreach (var item in doc.DocumentNode.SelectNodes("//a[@class='browse-movie-title']")) 
+            {
+                Console.WriteLine(item.InnerText);
+            }
+        }
+    }
+}
